2cd1f558a44bf82e5e928d71863da6c2bc7577e6
Felipe do Carmo Alvarenga <felipe.alvarenga@unifesp.br>
2025-09-11 06:44:51 -0300

    Create 1180c.c

---
+++ 1180c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int* Pint;
+
+int main (){
+  int n, i, k;
+  do{
+    scanf ("%d", &n);
+  }while (n < 1 || n > 1000);
+  Pint v = (Pint)malloc(n * sizeof(int));
+  for (i = 0; i < n; i++){
+    scanf ("%d", &v[i]);
+  }
+  if (v == NULL){
+    return 1;
+  }
+  k = (v + 0);
+  for (i = 0; i < n; i++){
+    if (k > *(v + i))
+      k = *(v + i);
+    else
+      continue;
+  }
+  printf ("Menor valor: %d\n", k);
+  for (i = 0; i < n; i++){
+    if (k == *(v + i))
+      break;
+    else
+      continue;
+  }
+  printf ("Posicao: %d\n", i);
+  return 0;
+  free (v);
+}
